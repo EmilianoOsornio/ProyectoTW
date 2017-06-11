87bbd09c74c3f8ee83212536f32d2984e8ecaf12
Yahir Trejo <yahardy58@gmail.com>
2017-06-11 18:59:58 -0500

    formato

--- ProyectoTW/web/js/grafica.js
+++ ProyectoTW/web/js/grafica.js
@@ -15,13 +15,7 @@ $("#p2b").val(-1);
 $(function() {
     obtenerPermisos("profesor");
     $("#btn-savePlot").click(function(e) {
-        /*
-        $("#bb1").val(bb1/b.zoomX);
-        $("#bb2").val(bb2/b.zoomY);
-        $("#bb3").val(bb3/b.zoomX);
-        $("#bb4").val(bb4/b.zoomY);
-        */
-        //alert(bb1);
+
         setCoordenadas();
         if(!$("#diagramaForm")[0].checkValidity()){
             $("#btn-submitPlot").click();
@@ -59,140 +53,7 @@ function setCoordenadas() {
     $("#bb4").val(bb4/b.zoomY);
 }
 
-$("#p1a").on("change keyup", function(event){
-        x = parseFloat($("#p1a").val());
-        y = parseFloat($("#p1b").val());
-        p1.setPosition(JXG.COORDS_BY_USER, [x,y]);
-        b.fullUpdate();
-        $("#x1").val($("#p1a").val());
-        $("#2x1").val($("#p1a").val());
-        $("#x2").val($("#p2a").val());
-        $("#m2").val($("#x2").val()-$("#x1").val());
-        $("#2m2").val($("#x2").val()-$("#x1").val());
-    });
-  
-    
-    $("#p1b").on("change keyup", function(event){
-        x = parseFloat($("#p1a").val());
-        y = parseFloat($("#p1b").val());
-        p1.setPosition(JXG.COORDS_BY_USER, [x,y]);
-        b.fullUpdate();
-        $("#y1").val($("#p1b").val());
-        $("#2y1").val($("#p1b").val());
-        $("#y2").val($("#p2b").val());
-        $("#m1").val($("#y2").val()-$("#y1").val());
-        $("#2m1").val($("#y2").val()-$("#y1").val());
-    });
-    
-    $("#p2a").on("change keyup", function(event){
-        x = parseFloat($("#p2a").val());
-        y = parseFloat($("#p2b").val());
-        p2.setPosition(JXG.COORDS_BY_USER, [x,y]);
-        $("#x1").val($("#p1a").val());
-        $("#x2").val($("#p2a").val());
-        $("#m2").val($("#x2").val()-$("#x1").val());
-        $("#2m2").val($("#x2").val()-$("#x1").val());
-        b.fullUpdate();
-    });
-    
-    $("#p2b").on("change keyup", function(event){
-        x = parseFloat($("#p2a").val());
-        y = parseFloat($("#p2b").val());
-        p2.setPosition(JXG.COORDS_BY_USER, [x,y]);
-        b.fullUpdate();
-        $("#y1").val($("#p1b").val());
-        $("#y2").val($("#p2b").val());
-        $("#m1").val($("#y2").val()-$("#y1").val());
-        $("#2m1").val($("#y2").val()-$("#y1").val());
-    });
-
-    $("#x1").on("change keyup", function(event){
-        x = parseFloat($("#x1").val());
-        y = parseFloat($("#y1").val());
-        p1.setPosition(JXG.COORDS_BY_USER, [x,y]);
-        b.fullUpdate();
-        $("#p1a").val($("#x1").val());
-        $("#2x1").val($("#x1").val());
-        $("#m2").val($("#x2").val()-$("#x1").val());
-        $("#2m2").val($("#x2").val()-$("#x1").val());
-    });
-    
-    $("#x2").on("change keyup", function(event){
-        x = parseFloat($("#x2").val());
-        y = parseFloat($("#y2").val());
-        p2.setPosition(JXG.COORDS_BY_USER, [x,y]);
-        b.fullUpdate();
-        $("#p2a").val($("#x2").val());
-        $("#m2").val($("#x2").val()-$("#x1").val());
-        $("#2m2").val($("#x2").val()-$("#x1").val());
-    });
-    
-    $("#y1").on("change keyup", function(event){
-        x = parseFloat($("#x1").val());
-        y = parseFloat($("#y1").val());
-        p1.setPosition(JXG.COORDS_BY_USER, [x,y]);
-        b.fullUpdate();
-        $("#p1b").val($("#y1").val());
-        $("#2y1").val($("#y1").val());
-        $("#m1").val($("#y2").val()-$("#y1").val());
-        $("#2m1").val($("#y2").val()-$("#y1").val());
-    });
-    
-    $("#y2").on("change keyup", function(event){
-        x = parseFloat($("#x2").val());
-        y = parseFloat($("#y2").val());
-        p2.setPosition(JXG.COORDS_BY_USER, [x,y]);
-        b.fullUpdate();
-        $("#p2b").val($("#y2").val());
-        $("#m1").val($("#y2").val()-$("#y1").val());
-        $("#2m1").val($("#y2").val()-$("#y1").val());
-    });
-    
-    $("#2y1").on("change keyup", function(event){
-        x = parseFloat($("#2x1").val());
-        y = parseFloat($("#2y1").val());
-        p1.setPosition(JXG.COORDS_BY_USER, [x,y]);
-        b.fullUpdate();
-        $("#p1b").val($("#2y1").val());
-        $("#y1").val($("#2y1").val());
-        $("#m1").val($("#y2").val()-$("#y1").val());
-        $("#2m1").val($("#y2").val()-$("#y1").val());
-    });
-    
-    $("#2x1").on("change keyup", function(event){
-        x = parseFloat($("#2x1").val());
-        y = parseFloat($("#2y1").val());
-        p1.setPosition(JXG.COORDS_BY_USER, [x,y]);
-        b.fullUpdate();
-        $("#p1a").val($("#2x1").val());
-        $("#x1").val($("#2x1").val());
-        $("#m2").val($("#x2").val()-$("#x1").val());
-        $("#2m2").val($("#x2").val()-$("#x1").val());
-    });
-    
-    p1.on('drag', function(){
-        $("#p1a").val(p1.coords.usrCoords[1]);
-        $("#p1b").val(p1.coords.usrCoords[2]);
-        $("#x1").val(p1.coords.usrCoords[1]);
-        $("#y1").val(p1.coords.usrCoords[2]);
-        $("#2y1").val(p1.coords.usrCoords[2]);
-        $("#2x1").val(p1.coords.usrCoords[1]);
-        $("#m1").val($("#y2").val()-$("#y1").val());
-        $("#2m1").val($("#y2").val()-$("#y1").val());
-        $("#m2").val($("#x2").val()-$("#x1").val());
-        $("#2m2").val($("#x2").val()-$("#x1").val());
-    });
-    
-    p2.on('drag', function(){
-        $("#p2a").val(p2.coords.usrCoords[1]);
-        $("#p2b").val(p2.coords.usrCoords[2]);
-        $("#x2").val(p2.coords.usrCoords[1]);
-        $("#y2").val(p2.coords.usrCoords[2]);
-        $("#m1").val($("#y2").val()-$("#y1").val());
-        $("#2m1").val($("#y2").val()-$("#y1").val());
-        $("#m2").val($("#x2").val()-$("#x1").val());
-        $("#2m2").val($("#x2").val()-$("#x1").val());
-    });
+setGraphListeners();
 
 
 
--- ProyectoTW/web/js/modificarDiagrama.js
+++ ProyectoTW/web/js/modificarDiagrama.js
@@ -10,6 +10,7 @@ var bb1=-5;
 var bb2=1;
 var bb3=5;
 var bb4=-1;
+
 $(function() {
     id = getUrlParameter('id');   
     console.log("Lo primero: "+id);
@@ -72,141 +73,7 @@ function createPlot(name,puntoA1,puntoB1,puntoA2,puntoB2,bb1,bb2,bb3,bb4){
     p2 = b.create('point',[num3,num4], {name:'B',size:4});
     var li = b.create('line',["A","B"], {strokeColor:'#00ff00',strokeWidth:2});
     
-    
-    $("#p1a").on("change keyup", function(event){
-        x = parseFloat($("#p1a").val());
-        y = parseFloat($("#p1b").val());
-        p1.setPosition(JXG.COORDS_BY_USER, [x,y]);
-        b.fullUpdate();
-        $("#x1").val($("#p1a").val());
-        $("#2x1").val($("#p1a").val());
-        $("#x2").val($("#p2a").val());
-        $("#m2").val($("#x2").val()-$("#x1").val());
-        $("#2m2").val($("#x2").val()-$("#x1").val());
-    });
-  
-    
-    $("#p1b").on("change keyup", function(event){
-        x = parseFloat($("#p1a").val());
-        y = parseFloat($("#p1b").val());
-        p1.setPosition(JXG.COORDS_BY_USER, [x,y]);
-        b.fullUpdate();
-        $("#y1").val($("#p1b").val());
-        $("#2y1").val($("#p1b").val());
-        $("#y2").val($("#p2b").val());
-        $("#m1").val($("#y2").val()-$("#y1").val());
-        $("#2m1").val($("#y2").val()-$("#y1").val());
-    });
-    
-    $("#p2a").on("change keyup", function(event){
-        x = parseFloat($("#p2a").val());
-        y = parseFloat($("#p2b").val());
-        p2.setPosition(JXG.COORDS_BY_USER, [x,y]);
-        $("#x1").val($("#p1a").val());
-        $("#x2").val($("#p2a").val());
-        $("#m2").val($("#x2").val()-$("#x1").val());
-        $("#2m2").val($("#x2").val()-$("#x1").val());
-        b.fullUpdate();
-    });
-    
-    $("#p2b").on("change keyup", function(event){
-        x = parseFloat($("#p2a").val());
-        y = parseFloat($("#p2b").val());
-        p2.setPosition(JXG.COORDS_BY_USER, [x,y]);
-        b.fullUpdate();
-        $("#y1").val($("#p1b").val());
-        $("#y2").val($("#p2b").val());
-        $("#m1").val($("#y2").val()-$("#y1").val());
-        $("#2m1").val($("#y2").val()-$("#y1").val());
-    });
-
-    $("#x1").on("change keyup", function(event){
-        x = parseFloat($("#x1").val());
-        y = parseFloat($("#y1").val());
-        p1.setPosition(JXG.COORDS_BY_USER, [x,y]);
-        b.fullUpdate();
-        $("#p1a").val($("#x1").val());
-        $("#2x1").val($("#x1").val());
-        $("#m2").val($("#x2").val()-$("#x1").val());
-        $("#2m2").val($("#x2").val()-$("#x1").val());
-    });
-    
-    $("#x2").on("change keyup", function(event){
-        x = parseFloat($("#x2").val());
-        y = parseFloat($("#y2").val());
-        p2.setPosition(JXG.COORDS_BY_USER, [x,y]);
-        b.fullUpdate();
-        $("#p2a").val($("#x2").val());
-        $("#m2").val($("#x2").val()-$("#x1").val());
-        $("#2m2").val($("#x2").val()-$("#x1").val());
-    });
-    
-    $("#y1").on("change keyup", function(event){
-        x = parseFloat($("#x1").val());
-        y = parseFloat($("#y1").val());
-        p1.setPosition(JXG.COORDS_BY_USER, [x,y]);
-        b.fullUpdate();
-        $("#p1b").val($("#y1").val());
-        $("#2y1").val($("#y1").val());
-        $("#m1").val($("#y2").val()-$("#y1").val());
-        $("#2m1").val($("#y2").val()-$("#y1").val());
-    });
-    
-    $("#y2").on("change keyup", function(event){
-        x = parseFloat($("#x2").val());
-        y = parseFloat($("#y2").val());
-        p2.setPosition(JXG.COORDS_BY_USER, [x,y]);
-        b.fullUpdate();
-        $("#p2b").val($("#y2").val());
-        $("#m1").val($("#y2").val()-$("#y1").val());
-        $("#2m1").val($("#y2").val()-$("#y1").val());
-    });
-    
-    $("#2y1").on("change keyup", function(event){
-        x = parseFloat($("#2x1").val());
-        y = parseFloat($("#2y1").val());
-        p1.setPosition(JXG.COORDS_BY_USER, [x,y]);
-        b.fullUpdate();
-        $("#p1b").val($("#2y1").val());
-        $("#y1").val($("#2y1").val());
-        $("#m1").val($("#y2").val()-$("#y1").val());
-        $("#2m1").val($("#y2").val()-$("#y1").val());
-    });
-    
-    $("#2x1").on("change keyup", function(event){
-        x = parseFloat($("#2x1").val());
-        y = parseFloat($("#2y1").val());
-        p1.setPosition(JXG.COORDS_BY_USER, [x,y]);
-        b.fullUpdate();
-        $("#p1a").val($("#2x1").val());
-        $("#x1").val($("#2x1").val());
-        $("#m2").val($("#x2").val()-$("#x1").val());
-        $("#2m2").val($("#x2").val()-$("#x1").val());
-    });
-    
-    p1.on('drag', function(){
-        $("#p1a").val(p1.coords.usrCoords[1]);
-        $("#p1b").val(p1.coords.usrCoords[2]);
-        $("#x1").val(p1.coords.usrCoords[1]);
-        $("#y1").val(p1.coords.usrCoords[2]);
-        $("#2y1").val(p1.coords.usrCoords[2]);
-        $("#2x1").val(p1.coords.usrCoords[1]);
-        $("#m1").val($("#y2").val()-$("#y1").val());
-        $("#2m1").val($("#y2").val()-$("#y1").val());
-        $("#m2").val($("#x2").val()-$("#x1").val());
-        $("#2m2").val($("#x2").val()-$("#x1").val());
-    });
-    
-    p2.on('drag', function(){
-        $("#p2a").val(p2.coords.usrCoords[1]);
-        $("#p2b").val(p2.coords.usrCoords[2]);
-        $("#x2").val(p2.coords.usrCoords[1]);
-        $("#y2").val(p2.coords.usrCoords[2]);
-        $("#m1").val($("#y2").val()-$("#y1").val());
-        $("#2m1").val($("#y2").val()-$("#y1").val());
-        $("#m2").val($("#x2").val()-$("#x1").val());
-        $("#2m2").val($("#x2").val()-$("#x1").val());
-    });
+    setGraphListeners();
 
     $("#btn-modifyPlot").click(function(e) {
         e.preventDefault();
